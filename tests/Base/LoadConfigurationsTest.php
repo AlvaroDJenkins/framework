f586cc173655ee03aa0b8927859a00e6afb5a201
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-09-12 13:11:13 +0530

    Update bootstrap tests to mock base application

--- tests/Base/LoadConfigurationsTest.php
+++ tests/Base/LoadConfigurationsTest.php
@@ -2,26 +2,76 @@
 
 namespace Emberfuse\Tests\Base;
 
+use Mockery as m;
 use Emberfuse\Tests\TestCase;
 use Emberfuse\Base\Application;
+use Emberfuse\Support\Repository;
 use Emberfuse\Base\Bootstrap\LoadConfigurations;
+use Emberfuse\Base\Contracts\ApplicationInterface;
+use Emberfuse\Support\Contracts\RepositoryInterface;
 
 class LoadConfigurationsTest extends TestCase
 {
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+
     public function testBootstrapConfigurations()
     {
-        $app = new Application(__DIR__ . '/fixtures');
+        $expectedData = [
+            'foo' => [
+                'bar' => 'baz',
+            ],
+        ];
         $configLoader = new LoadConfigurations();
-        $configLoader->bootstrap($app);
+        $configLoader->bootstrap($app = $this->getMockApp($expectedData));
 
         $this->assertTrue($app->has('config'));
-        $this->assertEquals(
-            [
-                'foo' => [
-                    'bar' => 'baz',
-                ],
-            ],
-            $app['config']->all()
-        );
+        $this->assertEquals($expectedData, $app['config']->all());
+    }
+
+    /**
+     * Get mocked Emberfuse application instance.
+     *
+     * @param array $expectedData
+     *
+     * @return \Emberfuse\Base\Contracts\ApplicationInterface
+     */
+    protected function getMockApp(array $expectedData): ApplicationInterface
+    {
+        $app = m::mock(Application::class);
+
+        $app->shouldReceive('instance')
+            ->once()
+            ->withAnyArgs()
+            ->andReturn();
+
+        $app->shouldReceive('basePath')
+            ->once()
+            ->with('config.yaml')
+            ->andReturn(__DIR__ . '/fixtures/config.yaml');
+
+        $app->shouldReceive('has')
+            ->once()
+            ->with('config')
+            ->andReturn(true);
+
+        $app->shouldReceive('offsetGet')
+            ->once()
+            ->with('config')
+            ->andReturn(new Repository($expectedData));
+
+        return $app;
+    }
+
+    /**
+     * Get instance of configurations repository.
+     *
+     * @return \Emberfuse\Support\Contracts\RepositoryInterface
+     */
+    protected function getConfigRepo(): RepositoryInterface
+    {
+        return new Repository([]);
     }
 }
